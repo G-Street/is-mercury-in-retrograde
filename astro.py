594c781c56be56ffe42dce12b065704159c07bc8
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-26 01:17:00 +1300

    Made alternate scripts

--- astro.py
+++ astro.py
@@ -3,13 +3,6 @@
 from skyfield.api import load
 import datetime, time, re
 
-# get time five seconds ago
-t1 = datetime.datetime.now() - datetime.timedelta(seconds=-5)
-precise_second_t1 = t1.strftime("%-S.%f")
-# get time now
-t2 = datetime.datetime.now()
-precise_second_t2 = t2.strftime("%-S.%f")
-
 # fetch data from the United States Naval Observatory and the International Earth Rotation Service
 planets = load('de421.bsp')
 # define planets earth and mercury
@@ -19,22 +12,28 @@ earth, mercury = planets['earth'], planets['mercury']
 ts = load.timescale()
 
 # Set times 1 and 2 as terrestrial time
-ttime1 = ts.utc(t1.year, t1.month, t1.day, t1.hour, t1.minute, float(precise_second_t1))
-ttime2 = ts.utc(t2.year, t2.month, t2.day, t2.hour, t2.minute, float(precise_second_t2))
+ttime1 = ts.now()
+time.sleep(7)
+ttime2 = ts.now()
 
 # sanity check for times 1 and 2 terrestrial time for when mercury is not in retrograde
-# ttime1 = ts.utc(2020, 1, 1, 12, 0, 0)
-# ttime2 = ts.utc(2020, 1, 1, 12, 5, 0)
+# ttime1 = ts.utc(2020, 1, 1, 12, 1, 0)
+# ttime2 = ts.utc(2020, 1, 1, 12, 1, 5)
 
 # get atrometric measurements from earth to mercury at times 1 and 2
 astrometric1 = earth.at(ttime1).observe(mercury)
 astrometric2 = earth.at(ttime2).observe(mercury)
 
-# set values from astrometric arrays
+"""
+ set values from astrometric arrays
+    ra = right ascension
+    dec = decline
+    ditance = distance
+"""
 ra1, dec1, distance1 = astrometric1.radec()
 ra2, dec2, distance2 = astrometric2.radec()
 
-# Split arrays
+# Split arrays to pull only the numeric values
 arr1 = re.sub(r'[a-zA-Z]+', '', str(ra1)).split()
 arr2 = re.sub(r'[a-zA-Z]+', '', str(ra2)).split()
 
@@ -44,9 +43,10 @@ ftr = [3600,60,1]
 time1_seconds = sum([a*b for a,b in zip(ftr, map(float,arr1))])
 time2_seconds = sum([a*b for a,b in zip(ftr, map(float,arr2))])
 
-# print(time2_seconds - time1_seconds)
-
-print('Then:', ra1)
-print('Now: ', ra2)
-# print('Then:', time1_seconds)
-# print('Now: ', time2_seconds)
+# interpret output of differences in RAs
+if float(time2_seconds - time1_seconds) < 0:
+    print("The right ascension of Mercury is negative: Mercury is in retrograde")
+elif float(time2_seconds - time1_seconds) > 0:
+    print("The right ascension of Mercury is positive: Mercury is not in retrograde")
+else:
+    print("The stars are not aligned.  I cannot tell if Mercury is in retrograde at the present time.  Please come back later.")
